af68b68b88c127a1660cdc1b4997a4093678853b
gniewkob <1093981+gniewkob@users.noreply.github.com>
2025-08-17 14:55:07 +0000

    chore: format code with prettier

--- frontend/.next/static/webpack/app/layout.5c70d9546ea9bff1.hot-update.js
+++ frontend/.next/static/webpack/app/layout.5c70d9546ea9bff1.hot-update.js
@@ -1,4 +1,4 @@
-"use strict";
+"use strict"
 /*
  * ATTENTION: An "eval-source-map" devtool has been used.
  * This devtool is neither made for production nor for readable output files.
@@ -7,16 +7,18 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-self["webpackHotUpdate_N_E"]("app/layout",{
-
-/***/ "(app-pages-browser)/./src/app/globals.css":
-/*!*****************************!*\
+self["webpackHotUpdate_N_E"]("app/layout", {
+  /***/ "(app-pages-browser)/./src/app/globals.css":
+    /*!*****************************!*\
   !*** ./src/app/globals.css ***!
   \*****************************/
-/***/ ((module, __webpack_exports__, __webpack_require__) => {
+    /***/ (module, __webpack_exports__, __webpack_require__) => {
+      eval(
+        __webpack_require__.ts(
+          '__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ("935f64cdb49e");\nif (true) { module.hot.accept() }\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL3NyYy9hcHAvZ2xvYmFscy5jc3MiLCJtYXBwaW5ncyI6Ijs7OztBQUFBLGlFQUFlLGNBQWM7QUFDN0IsSUFBSSxJQUFVLElBQUksaUJBQWlCIiwic291cmNlcyI6WyIvdXNyL2hvbWUvdmV0dGVybmtyYWZ0L2FwcHMvcHl0aG9uL3NjcmFwZXJfd29ya3NwYWNlL2Zyb250ZW5kL3NyYy9hcHAvZ2xvYmFscy5jc3MiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgXCI5MzVmNjRjZGI0OWVcIlxuaWYgKG1vZHVsZS5ob3QpIHsgbW9kdWxlLmhvdC5hY2NlcHQoKSB9XG4iXSwibmFtZXMiOltdLCJpZ25vcmVMaXN0IjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(app-pages-browser)/./src/app/globals.css\n',
+        ),
+      )
 
-eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (\"935f64cdb49e\");\nif (true) { module.hot.accept() }\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL3NyYy9hcHAvZ2xvYmFscy5jc3MiLCJtYXBwaW5ncyI6Ijs7OztBQUFBLGlFQUFlLGNBQWM7QUFDN0IsSUFBSSxJQUFVLElBQUksaUJBQWlCIiwic291cmNlcyI6WyIvdXNyL2hvbWUvdmV0dGVybmtyYWZ0L2FwcHMvcHl0aG9uL3NjcmFwZXJfd29ya3NwYWNlL2Zyb250ZW5kL3NyYy9hcHAvZ2xvYmFscy5jc3MiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgXCI5MzVmNjRjZGI0OWVcIlxuaWYgKG1vZHVsZS5ob3QpIHsgbW9kdWxlLmhvdC5hY2NlcHQoKSB9XG4iXSwibmFtZXMiOltdLCJpZ25vcmVMaXN0IjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(app-pages-browser)/./src/app/globals.css\n"));
-
-/***/ })
-
-});
+      /***/
+    },
+})
